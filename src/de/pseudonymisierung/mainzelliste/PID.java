9690223595da8a9665f8fcb44707a2092a8a5ef0
andiborg <borga@uni-mainz.de>
2015-03-18 18:31:24 +0100

    Updated year in copyright note.

--- src/de/pseudonymisierung/mainzelliste/PID.java
+++ src/de/pseudonymisierung/mainzelliste/PID.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Martin Lablans, Andreas Borg, Frank Ückert
+ * Copyright (C) 2013-2015 Martin Lablans, Andreas Borg, Frank Ückert
  * Contact: info@mainzelliste.de
  *
  * This program is free software; you can redistribute it and/or modify it under
