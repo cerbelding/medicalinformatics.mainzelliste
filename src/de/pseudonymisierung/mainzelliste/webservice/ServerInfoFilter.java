5c7363d57a73eb10bf2d69958bdca3618416983d
andiborg <borga@uni-mainz.de>
2015-12-22 18:56:30 +0100

    Added missing files.

---
+++ src/de/pseudonymisierung/mainzelliste/webservice/ServerInfoFilter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2013-2015 Martin Lablans, Andreas Borg, Frank Ückert
+ * Contact: info@mainzelliste.de
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free 
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or combining it 
+ * with Jersey (https://jersey.java.net) (or a modified version of that 
+ * library), containing parts covered by the terms of the General Public 
+ * License, version 2.0, the licensors of this Program grant you additional 
+ * permission to convey the resulting work.
+ */
+package de.pseudonymisierung.mainzelliste.webservice;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import de.pseudonymisierung.mainzelliste.Config;
+
+/**
+ * Adds header "Server" with application information to HTTP responses.
+ * The provided string is of the format "Mainzelliste/x.y.z". Activated by 
+ * filter definition "Mainzelliste - ServerInfoFilter" in web.xml.
+ */
+public class ServerInfoFilter implements Filter {
+
+	/**
+	 * Not used in this implementation.
+	 */
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+	/**
+	 * Not used in this implementation.
+	 */
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		if (response instanceof HttpServletResponse) {
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.addHeader("Server",
+					Config.instance.getUserAgentString());
+		}
+		chain.doFilter(request, response);
+	}
+}
