415f9fdb6f29d91e3e6f377df17470d81847995f
Christopher Hampf <c.hampf@arcor.de>
2016-09-23 12:48:25 +0200

    I hereby certify that my contribution meets the requirements stated by the Developer Certificate of Origin, version 1.1 (http://developercertificate.org/).

--- src/de/pseudonymisierung/mainzelliste/matcher/hasher/HashFormatter.java
+++ src/de/pseudonymisierung/mainzelliste/matcher/hasher/HashFormatter.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2013-2016 Martin Lablans, Andreas Borg, Frank Ückert and contributors
+ * (see Git commit history for individual contributions)
+ * Contact: info@mainzelliste.de
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free 
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or combining it 
+ * with Jersey (https://jersey.java.net) (or a modified version of that 
+ * library), containing parts covered by the terms of the General Public 
+ * License, version 2.0, the licensors of this Program grant you additional 
+ * permission to convey the resulting work.
+ */
 package de.pseudonymisierung.mainzelliste.matcher.hasher;
 
 import de.pseudonymisierung.mainzelliste.Config;
@@ -33,7 +59,6 @@ public class HashFormatter {
     @Column(name = "format", table = "HashFormatterList")
     private List<String> format;
 
-    /**  */
     @Transient
     private boolean formatChanged;
 
@@ -124,7 +149,7 @@ public class HashFormatter {
     }
 
     /**
-     * Initilize the attributes for checking whether the HashFormatter format
+     * Initialize the attributes for checking whether the HashFormatter format
      * was changing after a restart of Mainzelliste.
      * 
      * This call is only needed once.
