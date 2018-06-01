c09d023076a92f52503cf31cb95ffa8fe660482b
florian <florian.stampe@freenet.de>
2018-06-01 07:30:27 +0200

    Updated communicator structure, functions, documentation, added rest endpoints for callback and getAllRecords.

--- src/de/securerecordlinkage/Communicator.java
+++ src/de/securerecordlinkage/Communicator.java
@@ -1,5 +1,11 @@
 package de.securerecordlinkage;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 public class Communicator {
     // 2a. Re-send linkRecord to SRL
     // 2b. Process callback from SRL (linkRecord)
@@ -12,10 +18,40 @@ public class Communicator {
 
     }
 
-    // Send linkRecord to SRL
+
+
+    //-----------------------------------------------------------------------
+
+    /** send linkRecord, which should be linked, to SRL - In Architectur-XML (v6) step 2*/
     public void sendLinkRecord() {
 
     }
+
+    /** rest endpoint, used to set a linked record - In Architectur-XML (v6) step 7*/
+    @PUT
+    @Path("/linkCallBack")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Produces setLinkRecord(){
+
+        return null;
+    }
+
+
+    //-----------------------------------------------------------------------
+
+    /** return all entrys, which schould be compared, to SRL  - In Architectur-XML (v6) step 4*/
+    @GET
+    @Path("getAllRecords")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Produces getAllRecords(){
+
+        return null;
+    }
+
+
+
+
+
 
     // Process callback
     public void processCallback() {
