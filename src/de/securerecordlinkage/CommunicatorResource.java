682f29cb970f518f4562cb4b5b836f58e19e20bb
stampe <f.stampe@dkfz-heidelberg.de>
2018-06-01 10:28:41 +0200

    Rest-enpoint updates + Fake Json respone for test purpose

--- src/de/securerecordlinkage/CommunicatorResource.java
+++ src/de/securerecordlinkage/CommunicatorResource.java
@@ -1,12 +1,19 @@
 package de.securerecordlinkage;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-public class Communicator {
+@Path("Communicator")
+public class CommunicatorResource {
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
     // 2a. Re-send linkRecord to SRL
     // 2b. Process callback from SRL (linkRecord)
     // 4a. Process Request from SRL (getAllRecords)
@@ -24,6 +31,7 @@ public class Communicator {
 
     /** send linkRecord, which should be linked, to SRL - In Architectur-XML (v6) step 2*/
     public void sendLinkRecord() {
+        logger.info("sendLinkRecord");
 
     }
 
@@ -32,6 +40,7 @@ public class Communicator {
     @Path("/linkCallBack")
     @Produces(MediaType.APPLICATION_JSON)
     public Produces setLinkRecord(){
+        logger.info("setLinkRecord");
 
         return null;
     }
@@ -41,21 +50,33 @@ public class Communicator {
 
     /** return all entrys, which schould be compared, to SRL  - In Architectur-XML (v6) step 4*/
     @GET
-    @Path("getAllRecords")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Produces getAllRecords(){
+    @Path("/getAllRecords")
+    //@Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getAllRecords(){
+        logger.info("getAllRecords");
 
-        return null;
+        //return null;
+        return jsondummy();
     }
-
-
-
-
 
 
     // Process callback
     public void processCallback() {
         // Implemented in Class editID
+    }
+
+    private JSONObject jsondummy(){
+        JSONObject reqObject = new JSONObject();
+        JSONObject tmpObj = new JSONObject();
+        try {
+            tmpObj.put("authType", "apiKey");
+            tmpObj.put("sharedKey", "123abc");
+            reqObject.put("localAuthentification", tmpObj);
+        }catch (Exception e){
+            logger.info("jsondummy exception" + e.getMessage());
+        }
+
+        return  reqObject;
     }
 
 }
