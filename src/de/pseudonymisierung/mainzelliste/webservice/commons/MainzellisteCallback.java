aefc0f896e2d8c351d2b581fad0351b2eac1f672
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2019-10-10 14:13:54 +0200

    Applied builder pattern to MainzellisteCallback

--- src/de/pseudonymisierung/mainzelliste/webservice/commons/MainzellisteCallback.java
+++ src/de/pseudonymisierung/mainzelliste/webservice/commons/MainzellisteCallback.java
@@ -9,6 +9,7 @@ import de.pseudonymisierung.mainzelliste.webservice.AddPatientToken;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
@@ -26,6 +27,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -39,15 +41,22 @@ public class MainzellisteCallback {
     /** The TLS context depending on the configuration parameters */
     private SSLConnectionSocketFactory sslsf;
 
+    /** Api Version which should be used */
     private Servers.ApiVersion apiVersion;
 
+    // TODO: this could be checked to be a real url
+    /** The url to which the callback should be send */
+    private String url;
+    private HttpPost callbackRequest;
+
+    private String tokenId;
+    private List<ID> returnIds;
+
     /**
-     * represents the callback done by the request
-     *   TODO: describe behaviour of callback here
-     *
-     * @param apiVersion
+     * represents a callback executed by the Mainzelliste
+     *   TODO: describe behaviour of MainzellisteCallback here
      */
-    public MainzellisteCallback(Servers.ApiVersion apiVersion) {
+    public MainzellisteCallback() {
         try {
 
             SSLContextBuilder builder = new SSLContextBuilder();
@@ -71,64 +80,72 @@ public class MainzellisteCallback {
         } catch (KeyManagementException ex) {
             Logger.getLogger(PatientBackend.class).error("Error initializing client Transport Layer Security", ex);
         }
-        this.apiVersion = apiVersion;
     }
 
-    public void executeCallback(AddPatientToken t, List<ID> returnIds) {
-        String callback = t.getDataItemString("callback");
-        if (callback != null && callback.length() > 0) {
-            try {
-                logger.debug("Sending request to callback " + callback);
+    public MainzellisteCallback apiVersion(Servers.ApiVersion apiVersion){
+        this.apiVersion = apiVersion;
+        return this;
+    }
 
-                JSONObject reqBody = new JSONObject();
+    public MainzellisteCallback url (String url){
+        this.url = url;
+        return this;
+    }
 
-                if (apiVersion.majorVersion >= 2) {
-                    // Collect ids for Callback object
-                    JSONArray idsJson = new JSONArray();
+    public MainzellisteCallback tokenId (String tokenId){
+        this.tokenId = tokenId;
+        return this;
+    }
 
-                    for (ID thisID : returnIds) {
-                        idsJson.put(thisID.toJSON());
-                    }
+    public MainzellisteCallback returnIds (List<ID> returnIds){
+        this.returnIds = returnIds;
+        return this;
+    }
 
-                    reqBody.put("tokenId", t.getId())
-                            .put("ids", idsJson);
+    public HttpResponse execute() throws IOException {
+        HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        return httpClient.execute(this.callbackRequest);
+    }
 
-                } else {  // API version 1.0
-                    if (returnIds.size() > 1) {
-                        throw new WebApplicationException(
-                                Response.status(Response.Status.BAD_REQUEST)
-                                        .entity("Selected API version 1.0 permits only one ID in callback, " +
-                                                "but several were requested. Set mainzellisteApiVersion to a " +
-                                                "value >= 2.0 or request only one ID type in token.")
-                                        .build());
-                    }
-                    reqBody.put("tokenId", t.getId())
-                            .put("id", returnIds.get(0).getIdString());
-                }
+    public MainzellisteCallback build() throws JSONException, UnsupportedEncodingException {
+        HttpPost callbackReq = new HttpPost(this.url);
+        callbackReq.setHeader("Content-Type", MediaType.APPLICATION_JSON);
+        callbackReq.setHeader("User-Agent", Config.instance.getUserAgentString());
+        JSONObject reqBody = buildJson(this.tokenId, this.returnIds);
+        StringEntity reqEntity = new StringEntity(reqBody.toString());
+        reqEntity.setContentType("application/json");
+        callbackReq.setEntity(reqEntity);
+        this.callbackRequest = callbackReq;
+        return this;
+    }
 
-                HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-                HttpPost callbackReq = new HttpPost(callback);
-                callbackReq.setHeader("Content-Type", MediaType.APPLICATION_JSON);
-                callbackReq.setHeader("User-Agent", Config.instance.getUserAgentString());
-                StringEntity reqEntity = new StringEntity(reqBody.toString());
-                reqEntity.setContentType("application/json");
-                callbackReq.setEntity(reqEntity);
-                HttpResponse response = httpClient.execute(callbackReq);
-                StatusLine sline = response.getStatusLine();
-                // Accept callback if OK, CREATED or ACCEPTED is returned
-                if ((sline.getStatusCode() < 200) || sline.getStatusCode() >= 300) {
-                    logger.error("Received invalid status form mdat callback: " + response.getStatusLine());
-                    throw new InternalErrorException("Request to callback failed!");
-                }
-                // TODO: Server-Antwort auslesen
+    private JSONObject buildJson(String tokenId, List<ID> returnIds) throws JSONException {
+        // Building the JSON
+        JSONObject reqBody = new JSONObject();
 
-            } catch (JSONException e) {
-                logger.error("Internal serializitaion error: ", e);
-                throw new InternalErrorException("Internal serializitaion error!");
-            } catch (IOException e) {
-                logger.error("Internal error building the request: ", e);
-                throw new InternalErrorException("Internal error building the request!");
+        if (apiVersion.majorVersion >= 2) {
+            // Collect ids for Callback object
+            JSONArray idsJson = new JSONArray();
+
+            for (ID thisID : returnIds) {
+                idsJson.put(thisID.toJSON());
             }
+
+            reqBody.put("tokenId", tokenId)
+                    .put("ids", idsJson);
+
+        } else {  // API version 1.0
+            if (returnIds.size() > 1) {
+                throw new WebApplicationException(
+                        Response.status(Response.Status.BAD_REQUEST)
+                                .entity("Selected API version 1.0 permits only one ID in callback, " +
+                                        "but several were requested. Set mainzellisteApiVersion to a " +
+                                        "value >= 2.0 or request only one ID type in token.")
+                                .build());
+            }
+            reqBody.put("tokenId", tokenId)
+                    .put("id", returnIds.get(0).getIdString());
         }
+        return reqBody;
     }
 }
