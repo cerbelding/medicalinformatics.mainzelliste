0d8b6efbeb38ce7cff36f35963bbd16c8f2e67d9
Florian Stampe <f.stampe@dkfz-heidelberg.de>
2019-10-16 15:11:22 +0200

    Add redirect utilities

---
+++ src/de/pseudonymisierung/mainzelliste/webservice/common/Redirect.java
@@ -0,0 +1,38 @@
+package de.pseudonymisierung.mainzelliste.webservice.common;
+
+import com.sun.jersey.api.uri.UriTemplate;
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.Map;
+
+public class Redirect {
+
+    private Logger logger = Logger.getLogger(Redirect.class);
+
+
+    private Map<String, String> tokenId;
+    private Map<String, String> mappedIdTypesdAndIds;
+    private UriTemplate uriTemplate;
+    private URI redirectURI;
+
+    private Map<String, String> allMappedValues;
+
+    public Redirect(Map<String, String> tokenId, Map<String, String> mappedIdTypesdAndIds, UriTemplate uriTemplate, URI redirectURI, Map<String, String> allMappedValues) {
+        this.tokenId = tokenId;
+        this.mappedIdTypesdAndIds = mappedIdTypesdAndIds;
+        this.uriTemplate = uriTemplate;
+        this.redirectURI = redirectURI;
+        this.allMappedValues = allMappedValues;
+    }
+
+    public Response execute(){
+        logger.info("execute() " + "redirectURI: " + this.redirectURI);
+        return Response.status(javax.ws.rs.core.Response.Status.SEE_OTHER)
+                .location(this.redirectURI)
+                .build();
+    }
+
+
+}
