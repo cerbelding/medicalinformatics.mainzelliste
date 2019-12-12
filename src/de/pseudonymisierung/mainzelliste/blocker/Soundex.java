1f1df32dc04b7d6fc9b38809c003fbcd8f29d6ee
Florens Rohde <florens.rohde@uni-leipzig.de>
2019-12-12 15:38:06 +0100

    Replace custom Soundex encoding

--- src/de/pseudonymisierung/mainzelliste/blocker/Soundex.java
+++ src/de/pseudonymisierung/mainzelliste/blocker/Soundex.java
@@ -20,6 +20,8 @@ import java.util.Set;
  */
 public class Soundex extends BlockingKeyExtractor {
 
+	private static final org.apache.commons.codec.language.Soundex encoder = new org.apache.commons.codec.language.Soundex();
+
 	public Soundex(String name, Map<String, String> parameters) {
 		super(name, parameters);
 	}
@@ -59,61 +61,7 @@ public class Soundex extends BlockingKeyExtractor {
 
 	private static Optional<String> computeSoundex(String s) {
 		if (s == null || s.length() <= 0) return Optional.empty();
-		char[] x = s.toUpperCase().toCharArray();
-		char firstLetter = x[0];
-		for (int i = 0; i < x.length; i++) {
-			switch (x[i]) {
-				case 'B':
-				case 'F':
-				case 'P':
-				case 'V':
-					x[i] = '1';
-					break;
-
-				case 'C':
-				case 'G':
-				case 'J':
-				case 'K':
-				case 'Q':
-				case 'S':
-				case 'X':
-				case 'Z':
-					x[i] = '2';
-					break;
-
-				case 'D':
-				case 'T':
-					x[i] = '3';
-					break;
-
-				case 'L':
-					x[i] = '4';
-					break;
-
-				case 'M':
-				case 'N':
-					x[i] = '5';
-					break;
-
-				case 'R':
-					x[i] = '6';
-					break;
-
-				default:
-					x[i] = '0';
-					break;
-			}
-		}
-
-		// remove duplicates
-		String output = "" + firstLetter;
-		for (int i = 1; i < x.length; i++)
-			if (x[i] != x[i - 1] && x[i] != '0')
-				output += x[i];
-
-		// pad with 0's or truncate
-		output = output + "0000";
-		return Optional.of(output.substring(0, 4));
+		return Optional.ofNullable(encoder.encode(s));
 	}
 
 	@Override
