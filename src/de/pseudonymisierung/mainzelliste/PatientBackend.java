cf19b9169fc92e0d2228dcb4518afc37f3e21915
Torben Brenner <torben.brenner+accounts@googlemail.com>
2020-04-06 14:18:48 +0200

    Changed condition before matching idat on matching external ids

--- src/de/pseudonymisierung/mainzelliste/PatientBackend.java
+++ src/de/pseudonymisierung/mainzelliste/PatientBackend.java
@@ -400,7 +400,7 @@ public enum PatientBackend {
                 boolean idMatchHasIdat = Validator.instance.getRequiredFields().stream().allMatch(f ->
                         idMatch.getFields().containsKey(f) && !idMatch.getFields().get(f).isEmpty());
                 // Check if IDAT of input and match (if present) matches
-                if (inputPatient.getFields().isEmpty() && idMatchHasIdat) {
+                if (!inputPatient.getFields().isEmpty() && idMatchHasIdat) {
                     MatchResult matchWithIdMatch = Config.instance.getMatcher().match(inputPatient, Collections.singletonList(idMatch));
                     if (matchWithIdMatch.getResultType() != MatchResultType.MATCH) {
                         throw new WebApplicationException(
